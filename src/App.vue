2577d099d25ea5d8459b48f68434ba12735a6dd8
Connor Leech <connor@stitchlabs.com>
2018-10-11 10:15:41 -0700

    render and delete tasks

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
 <template>
     <div>
         <h1>Task List</h1>
+        <ul class="list-group">
+            <li class="list-group-item d-flex justify-content-between align-items-center" v-for="task in tasks">
+                {{ task.name }}
+                <span @click='deleteTask(task._id)' class="badge badge-danger badge-pill">X</span>
+            </li>
+        </ul>
     </div>
 </template>
 <style>
@@ -8,10 +14,14 @@
 </style>
 <script>
   import axios from 'axios';
+  import _ from 'lodash';
 
   export default {
-    created(){
-
+    created() {
+      axios.get(`/api/task`)
+        .then((response) => {
+          this.tasks = response.data;
+        });
     },
     data() {
       return {
@@ -19,10 +29,15 @@
       }
     },
     methods: {
-     
+      deleteTask(taskId) {
+        axios.delete(`/api/task/${taskId}`)
+          .then((response) => {
+            this.tasks = _.remove(this.tasks, (task) => {
+              return task._id !== taskId;
+            });
+          });
+      }
     },
-    computed: {
-
-    }
+    computed: {}
   }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import axios from 'axios'
+
+axios.defaults.baseURL = 'http://localhost:3000';
 
 new Vue({
   el: '#app',
   render: h => h(App)
-})
+});
